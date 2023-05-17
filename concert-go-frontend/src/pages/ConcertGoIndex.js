4fb575f6c1c2c0c9fd8ae88433359c8d448bffcb
FLopez73 <flopez730@gmail.com>
2023-05-17 15:25:55 -0700

    index testing

--- concert-go-frontend/src/pages/ConcertGoIndex.js
+++ concert-go-frontend/src/pages/ConcertGoIndex.js
@@ -1,66 +1,64 @@
-import { useState } from "react";
+import { useState } from "react"
 import {
-	Carousel,
-	CarouselItem,
-	CarouselControl,
-	CarouselIndicators,
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
   Card,
   CardBody,
   CardTitle,
   CardSubtitle,
   CardText,
-  Button
-} from "reactstrap";
-
-
-
+  Button,
+} from "reactstrap"
 
 const ConcertGoIndex = ({ args, currentEvent }) => {
-  return(
+  return (
     <>
-    
-    <div className="grid grid-cols-3 gap-8">
-      { currentEvent?.map(({ currentEvent, city, state, venue, artist, date, show_time, genre, images, id }) => {
-        return(
-          <Card key={ currentEvent }
-  style={{
-    width: '18rem'
-  }}
->
-  <img
-    alt="Concert Image"
-    src= { images }
-  />
-  <CardBody>
-    <CardTitle tag="h5">
-      { artist }
-    </CardTitle>
-    <CardSubtitle
-      className="mb-2 text-muted"
-      tag="h6"
-    >
-      { genre } 
-    </CardSubtitle>
-    <CardText>
-      { venue }
-      <br/>
-      { city }, { state } 
-      <br/> 
-      { date } @ { show_time }
-    </CardText>
-    <Button>
-      View Event
-    </Button>
-  </CardBody>
-</Card>
-        )
-
-      })}
-    </div>
-    
+      <h1>Event List</h1>
+      <div className="grid grid-cols-3 gap-8">
+        {currentEvent?.map(
+          ({
+            currentEvent,
+            city,
+            state,
+            venue,
+            artist,
+            date,
+            show_time,
+            genre,
+            images,
+            id,
+          }) => {
+            return (
+              <Card
+                key={currentEvent}
+                style={{
+                  width: "18rem",
+                }}
+              >
+                <img alt="Concert Image" src={images} />
+                <CardBody>
+                  <CardTitle tag="h5">{artist}</CardTitle>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    {genre}
+                  </CardSubtitle>
+                  <CardText>
+                    {venue}
+                    <br />
+                    {city}, {state}
+                    <br />
+                    {date} Doors Open @ {show_time}
+                  </CardText>
+                  <Button>View Event</Button>
+                </CardBody>
+              </Card>
+            )
+          }
+        )}
+      </div>
     </>
   )
 }
-	
 
-export default ConcertGoIndex;
+export default ConcertGoIndex
