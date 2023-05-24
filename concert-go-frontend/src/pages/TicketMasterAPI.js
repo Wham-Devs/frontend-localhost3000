b9fff279221724851d1d6fc6d25a4f7270b735af
JAKEDAHLGREN <Bigbear0649@gmail.com>
2023-05-24 10:15:30 -0700

    attribute calls from ticketmaster

--- concert-go-frontend/src/pages/TicketMasterAPI.js
+++ concert-go-frontend/src/pages/TicketMasterAPI.js
@@ -2,27 +2,27 @@ import React from "react";
 import { Button } from "reactstrap";
 
 const TicketMasterAPI = ({ handleSubmit, currentEvent }) => {
+	const events = currentEvent._embedded && currentEvent._embedded.events;
+
 	return (
 		<div>
 			TicketMasterAPI
 			<Button value="button" onClick={handleSubmit}>
-				Push Me
+				Push Me Papi
 			</Button>
-			{currentEvent &&
-				currentEvent._embedded.events.map(({ index, events, images, name, state, id }) => {
-					return (
-						<div>
-              <ul>
-<li key={index}>{name}</li>
- <li key={index}>{state}</li>
-<li key={index}><img src={images[0].url} alt="Concert test" /></li>
-              </ul>
-							
-             
-							
-						</div>
-					);
-				})}
+			{events &&
+				events.map((event) => (
+					<div key={event.id}>
+						<h1>{event.name}</h1>
+						<img src={event.images[0].url} alt="Event" />
+						<p>Genre: {event.classifications[0].genre.name}</p>
+						<p>Date: {event.dates.start.localDate}</p>
+						<p>Showtime: {event.dates.start.localTime}</p>
+            <p>Venue: {event._embedded.venues[0].name}</p>
+            <p>City: {event._embedded.venues[0].city.name}</p>
+            <p>State: {event._embedded.venues[0].state.name}</p>
+					</div>
+				))}
 		</div>
 	);
 };
